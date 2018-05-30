f3243bd5b26ef049b17a59b30ffb9b2eba01f60b
evagolabek <golabek.eva@gmail.com>
2018-05-30 15:56:27 +0200

    better version

--- src/App.js
+++ src/App.js
@@ -6,28 +6,35 @@ import Main from './components/Main'
 import renderHTML from 'react-render-html'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <Router>
         <MuiThemeProvider>
           <div className="App">
-            <div className="title">Eva Golabek-Puget</div>
-            <div className="nav-bar"></div>
-            <div className='side-bar'>
-              <div className='profile-img'>
-                <img src={'/images/me.png'} alt="" />
+            <div className='content'>
+
+
+              <div className="nav-bar">
+
+
+                <div className='profile-img'>
+                  <img src={'/images/me.png'} alt="" /></div>
+                <div className="title"> Eva Golabek-Puget</div>
+                <div className='about'>
+                  <br/>
+                  I am a junior web developer with previous experience and interest in fashion and marketing.
+                  I have strong interpersonal skills developed both within education and work.
+                  My other strengths are that I am a fast learner, highly self-motivated and adaptable to changing circumstances and environments
+                </div>
+                
+                <div className="linkedin">
+                  {renderHTML(`<a href="https://www.linkedin.com/in/eva-golabek-puget-24303840/">LinkedIn</a>`)}
+                </div>
               </div>
-              <div className = 'about'>
-                <br/>
-                <text> I am a junior web developer with previous experience and interest in fashion and marketing.
-                I have strong interpersonal skills developed both within education and work.
-                My other strengths are that I am a fast learner, highly self-motivated and adaptable to changing circumstances and environments.</text>
+              <div className="projects">
+                <Route exact path="/" component={Main} />
               </div>
-              <br/>
-              {renderHTML(`<a href="https://www.linkedin.com/in/eva-golabek-puget-24303840/">LinkedIn</a>`)}
-            </div>
-            <div className='projects'>
-              <Route exact path="/" component={Main} />
+
             </div>
           </div>
         </MuiThemeProvider>
@@ -37,3 +44,37 @@ class App extends Component {
 }
 
 export default App;
+// class App extends Component {
+//   render() {
+//     return (
+//       <Router>
+//         <MuiThemeProvider>
+//           <div className="App">
+//             <div className="nav-bar"></div>
+//             <div className='side-bar'>
+//               <div className='profile-img'>
+//                 <img src={'/images/me.png'} alt="" />
+//               </div>
+//               <div className="title">Eva Golabek-Puget</div>
+//               <div className = 'about'>
+//                 <br/>
+//                 <text> I am a junior web developer with previous experience and interest in fashion and marketing.
+//                 I have strong interpersonal skills developed both within education and work.
+//                 My other strengths are that I am a fast learner, highly self-motivated and adaptable to changing circumstances and environments.</text>
+//               </div>
+//               <br/>
+//               <div className = 'linkedin'>
+//               {renderHTML(`<a href="https://www.linkedin.com/in/eva-golabek-puget-24303840/">LinkedIn</a>`)}
+//               </div>
+//             </div>
+//             <div className='projects'>
+//               <Route exact path="/" component={Main} />
+//             </div>
+//           </div>
+//         </MuiThemeProvider>
+//       </Router>
+//     );
+//   }
+// }
+//
+// export default App;
